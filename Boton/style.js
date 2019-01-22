a8604fa6f89453e174b2ce1ba643e4a7a7e5a9cb
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-22 11:19:00 -0500

    Paso 2: Importar las librerías que se necesitarán y llamar los estilos separado

---
+++ Boton/style.js
@@ -0,0 +1,4 @@
+import React, { Component, PropTypes } from 'react'; 
+import { Text, TouchableOpacity, View, } from 'react-native'; 
+import Base, { Default, Danger, Info, Success } from './ styles';
+
