716304b26f0197898a2ddafc81780da80ef56e88
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-22 15:12:41 -0500

    Paso 10: Declarando los estilos del botón, primero los queserán la Base.

--- Boton/style.js
+++ Boton/style.js
@@ -1,1 +1,15 @@
+// Paso 10: Declarando los estilos del botón, primero los queserán la Base.
+import { StyleSheet } from 'react-native';
 
+const BaseStyles = StyleSheet.create({
+  main: {
+    padding: 10,
+    borderRadius: 3,
+  },
+  label: {
+    color: '#fff',
+  },
+  rounded: {
+    borderRadius: 20,
+  },
+});
