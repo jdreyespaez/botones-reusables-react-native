83a63312cf982cbadda0ac7efc51d0e41f02cf8b
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-22 11:35:43 -0500

    Paso 3: Definimos la clase de este componente definiremos las propiedades en propTypes y usaremos dos métodos getTheme() y render()

--- Boton/index.js
+++ Boton/index.js
@@ -1,3 +1,21 @@
 import React, { Component, PropTypes } from 'react'; 
 import { Text, TouchableOpacity, View, } from 'react-native'; 
 import Base, { Default, Danger, Info, Success } from './ styles';
+
+const { array, string, object, bool, func, any } = PropTypes;
+
+class Boton extends Component {
+    static propTypes = {
+        // Se definirán en el Paso 4.
+    }
+    getTheme() {
+        // Se definirá en el Paso 5.
+    }
+
+    render() {
+        // Se definirá en el Paso 6.
+    }
+};
+
+export default Boton;
+
