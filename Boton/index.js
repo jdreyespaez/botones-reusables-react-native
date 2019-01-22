a3d6b703146265240f23aa43c607a0cefa8b0f01
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-22 15:24:15 -0500

    Arreglando la ruta correcta para llamar los estilos

--- Boton/index.js
+++ Boton/index.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'; 
 import { Text, TouchableOpacity, View, } from 'react-native'; 
-import Base, { Default, Danger, Info, Success } from './ styles';
+import Base, { Default, Danger, Info, Success } from './styles';
 
 const { array, string, object, bool, func, any } = PropTypes;
 
