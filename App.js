6e7f8f54d830f7044e3eaa0a51328764a394aff7
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-22 12:12:19 -0500

    Paso 8: Definiremos un componente sin estado (stateless) y que mostrará algunos botones.

--- App.js
+++ App.js
@@ -3,6 +3,21 @@ import { StyleSheet, Alert, View } from 'react-native';
 import Boton from '.Boton';
 
 export default class App extends React.Component {
+  
+  // Paso 8:
+  function onPressBtn() {
+    Alert.alert('Alert', 'You clicked this button!');
+  }
+
+  const MainApp  = () => (
+    <View style={styles.container}>
+      <Button style={styles.btn}> My first button </Button>
+      <Button success style={styles.btn}> Success button </Button>
+      <Button info style={styles.btn}> Info button </Button>
+      <Button danger rounded style={styles.btn} onPress={onPressBtn}> Rounded button </Button>
+    </View>
+  );
+  
   render() {
     return (
 
